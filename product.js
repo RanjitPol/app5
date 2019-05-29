55d204a49ad19e0dbd5efdad83940c3e5c839503
Ranjit <ranjitpol.rp.com>
2019-05-29 15:50:36 +0530

    add GET product route

--- product.js
+++ product.js
@@ -13,51 +13,51 @@ router.get('/product', (request, response) => {
     });
 });
 
-router.post('/product', (request, response) => {
-    const title = request.body.title;
-    const description = request.body.description;
-    const price = request.body.price;
-    const rating = request.body.rating;
+// router.post('/product', (request, response) => {
+//     const title = request.body.title;
+//     const description = request.body.description;
+//     const price = request.body.price;
+//     const rating = request.body.rating;
 
-    const statement = `insert into Product 
-        (title, description, price, rating) values
-        ('${title}', '${description}', ${price}, ${rating})`;
-    const connection = db.connect();
-    connection.query(statement, (error, products) => {
-        connection.end();
-        response.send(utils.createResponse(error, products));
-    });
-});
+//     const statement = `insert into Product 
+//         (title, description, price, rating) values
+//         ('${title}', '${description}', ${price}, ${rating})`;
+//     const connection = db.connect();
+//     connection.query(statement, (error, products) => {
+//         connection.end();
+//         response.send(utils.createResponse(error, products));
+//     });
+// });
 
-router.put('/product/:id', (request, response) => {
-    const id = request.params.id;
-    const title = request.body.title;
-    const description = request.body.description;
-    const price = request.body.price;
-    const rating = request.body.rating;
+// router.put('/product/:id', (request, response) => {
+//     const id = request.params.id;
+//     const title = request.body.title;
+//     const description = request.body.description;
+//     const price = request.body.price;
+//     const rating = request.body.rating;
     
-    const statement = `
-        update Product set 
-            title = '${title}',
-            description = '${description}',
-            price = ${price},
-            rating = ${rating}
-        where id = ${id}`;product
-    const connection = db.connect();
-    connection.query(statement, (error, products) => {
-        connection.end();
-        response.send(utils.createResponse(error, products));
-    });
-});
+//     const statement = `
+//         update Product set 
+//             title = '${title}',
+//             description = '${description}',
+//             price = ${price},
+//             rating = ${rating}
+//         where id = ${id}`;product
+//     const connection = db.connect();
+//     connection.query(statement, (error, products) => {
+//         connection.end();
+//         response.send(utils.createResponse(error, products));
+//     });
+// });
 
-router.delete('/product/:id', (request, response) => {
-    const id = request.params.id;
-    const statement = `delete  from  Product where id = ${id}`;
-    const connection = db.connect();
-    connection.query(statement, (error, products) => {
-        connection.end();
-        response.send(utils.createResponse(error, products));
-    });
-});
+// router.delete('/product/:id', (request, response) => {
+//     const id = request.params.id;
+//     const statement = `delete  from  Product where id = ${id}`;
+//     const connection = db.connect();
+//     connection.query(statement, (error, products) => {
+//         connection.end();
+//         response.send(utils.createResponse(error, products));
+//     });
+// });
 
 module.exports = router;
